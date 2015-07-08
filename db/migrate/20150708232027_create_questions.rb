0dd83cebab540aab4bab58b0ea46c23b4f48fd7e
Lowell Mower <lowell.m.mower@gmail.com>
2015-07-08 19:30:15 -0400

    add rows for questions, votes, comments, answers

--- db/migrate/20150708232027_create_questions.rb
+++ db/migrate/20150708232027_create_questions.rb
@@ -1,6 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
+      t.text :text, null: false
+      t.references :user
 
       t.timestamps null: false
     end
--- db/migrate/20150708232035_create_answers.rb
+++ db/migrate/20150708232035_create_answers.rb
@@ -1,6 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
+      t.text :text, null: false
+      t.references :question
 
       t.timestamps null: false
     end
