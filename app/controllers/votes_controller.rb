dcb726f5d6246c4fa455d93b36b144d7595e923b
John Doe <benjamin.cohen1989@gmail.com>
2015-07-10 16:24:51 -0400

    fixing last bit

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,21 @@
+class VotesController < ApplicationController
+
+  def create
+    if params["type"] == "question"
+      @question = Question.find(params[:question_id])
+      @question.votes.new
+      @question.votes.last.user_id = session[:user_id]
+      @question.votes.last.save
+      if params["direction"] == "up"
+        @question.total += 1
+      else
+        @question.total -= 1
+      end
+      @question.save
+      redirect_to question_path(@question.id)
+    else
+
+    end
+  end
+
+end
