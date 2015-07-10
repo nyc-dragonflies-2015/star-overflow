08155f093235a9dec5bb8754a97ad84fa52058ad
Lowell Mower <lowell.m.mower@gmail.com>
2015-07-10 13:02:23 -0400

    add delete method for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,20 @@
 class CommentsController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
-    @comment = @question.comments.new(comment_params)
+    @comment = @question.comments.new(text:     comment_params[:text],
+                                      user_id:  session[:user_id])
     if @comment.save
       redirect_to @question
     else
       redirect_to root_path
     end
+  end
+
+  def destroy
+    @question = Question.find(params[:question_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to @question
   end
 
   private
