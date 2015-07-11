5ac9ce6a1ab05ab7adbab5b62457d6ddbfb5823a
Lowell Mower <lowell.m.mower@gmail.com>
2015-07-11 10:30:01 -0400

    change root index to be questions index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  root 'welcome#index'
+  root 'questions#index'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
