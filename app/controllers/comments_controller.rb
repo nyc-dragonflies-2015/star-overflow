036b0548a70ca8de1a5ddd0fb261ba03a8c5ddae
Lowell Mower <lowell.m.mower@gmail.com>
2015-07-11 09:37:13 -0400

    updated create method for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,13 @@
 class CommentsController < ApplicationController
   def create
-    @question = Question.find(params[:question_id])
-    @comment = @question.comments.new(text:     comment_params[:text],
-                                      user_id:  session[:user_id])
+    if params[:answer_id]
+      @answer = Answer.find(params[:answer_id])
+      @question = Question.find(@answer.question_id)
+      @comment = @answer.comments.new(text: comment_params[:text], user_id: session[:user_id])
+    else
+      @question = Question.find(params[:question_id])
+      @comment = @question.comments.new(text: comment_params[:text], user_id: session[:user_id])
+    end
     if @comment.save
       redirect_to @question
     else
